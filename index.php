b5b82ed67433bc53c1b23408b7b99e34ee161185
Negeye <ahmednfarah@gmail.com>
2019-06-01 13:08:34 +0300

    Official look intact, masonry working but breaks after clicking on filter

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Nenomia</title>
+	<link rel="stylesheet" href="css/uikit.min.css" />
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="style.css" />
+	<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+	<script defer src="/js/all.js"></script>
+</head>
+<body>
+
+	<div class="uk-section uk-section-default uk-section-small" id="main">
+		<div class="uk-container uk-container-small">
+			<div uk-grid uk-filter="target: .js-filter">
+				<div class="uk-width-1-1 uk-width-1-3@s uk-width-1-6@m">
+					<ul class="uk-list uk-list-nav uk-link-text uk-text-right">
+						<a class="uk-logo" href="/">
+							<img data-src="/img/logo.png" alt="Nenomia" uk-img>
+						</a>
+						<hr class="uk-divider-small">
+						<li class="neno-filter" uk-filter-control><a class="uk-link-reset" href="#">All</a></li>
+						<li class="neno-filter" uk-filter-control="filter: .tag-white"><a class="uk-link-reset" href="#">Timelines</a></li>
+						<li class="neno-filter" uk-filter-control="filter: .tag-blue"><a class="uk-link-reset" href="#">Key Players</a></li>
+					</ul>
+					<div class="uk-section uk-section-default uk-section-xsmall" id="footer">
+						<div class="uk-container">
+							<p class="uk-text-right">
+								<a href="#" class="uk-margin-small-right" uk-icon="whatsapp"></a>
+								<a href="#" class="uk-margin-small-right" uk-icon="youtube"></a>
+								<a href="#" class="uk-margin-small-right" uk-icon="facebook"></a>
+							</p>
+						</div>
+					</div> <!-- Footer section -->
+				</div>
+
+				<div class="uk-width-1-1 uk-width-2-3@s uk-width-5-6@m">
+					<div class="js-filter grid uk-text-center">
+						<div class="grid-sizer"></div>
+						<div class="grid-item tag-white">
+							<div href="#" class="neno-card">
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="https://images.unsplash.com/photo-1559302995-ab792ee16ce8?ixlib=rb-1.2.1&auto=format&fit=crop&w=614&q=80" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consecteturconsectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-blue">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="/img/favicon.png" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur, sed do eiusmod tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-white">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="https://images.unsplash.com/photo-1559257153-ab1781ff8d94?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod consectetur consectetur consectetur tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-white">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="https://images.unsplash.com/photo-1559255394-044328bfa8b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=635&q=80" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor consectetur consectetur incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-blue">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="https://images.unsplash.com/photo-1559250543-36c18ccff71d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-black">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="https://images.unsplash.com/photo-1559255394-d92ddc3250e5?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do consectetur eiusmod tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-blue">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="https://images.unsplash.com/photo-1559251606-c623743a6d76?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur sed do eiusmod tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-white">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="/img/favicon.png" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do consectetur consectetur eiusmod tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-blue">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="/img/favicon.png" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing consectetur consectetur elit, sed do eiusmod tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid-item tag-black">
+							<div>
+								<div class="uk-card uk-card-default">
+									<div class="uk-card-media-top">
+										<img src="/img/favicon.png" alt="">
+									</div>
+									<div class="uk-card-body">
+										<h3 class="uk-card-title">Media Top</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur, sed do eiusmod tempor incididunt.</p>
+										<a href="#" class="neno-link"><i class="fas fa-outdent"></i> The Placeholder Timeline</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</ul>
+				</div>
+			</div>
+		</div> <!-- Main container -->
+	</div> <!-- Main section -->
+
+	<footer class="uk-text-center">
+		<?php echo '&copy; Nenomia | '.date('Y');?>
+	</footer>
+
+	<script src="js/jquery-3.3.1.js"></script>
+	<script src="js/uikit.min.js"></script>
+	<script src="js/uikit-icons.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/masonry.pkgd.min.js"></script>
+	<script src="js/imagesloaded.pkgd.min.js"></script>
+	<script src="js/script.js"></script>
+
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -2,4 +2,14 @@ $(document).ready(function () {
   $('body').animate({
     'opacity': '1'
   }, 500);
+
+  var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      gutter: 37
+    });
+  });
+  
 });
